16aed9b32f4d84e5bf6e9d76d91cb2fda571e16d
maxencetauzin <maxence.tauzin@gmail.com>
2022-03-31 09:18:55 +0200

    Change form and validation

--- src/demandeFormation/DemandeFormation.js
+++ src/demandeFormation/DemandeFormation.js
@@ -1,31 +1,43 @@
 import React, { Component } from 'react';
 import { default as ReactSelect,components } from "react-select";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+
 class DemandeFormation extends Component {
-  static propTypes = {
-  };
-  constructor(props){
-      super(props);   
-      this.state={
-          showModalConfirmation:false,
-          selectedOption:null,
-          sujet:null,
-          detailDemande:null,
-          validated:false
-      }
+  constructor() {
+    super();
+    this.state = {
+      input: {},
+      errors: {},
+      showModalConfirmation:false
+    };
+    this.ChildElement = React.createRef();
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleClickForm(event){
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+     
+  handleChange(event) {
+    let input = this.state.input;
+    input[event.target.name] = event.target.value;
+  
+    this.setState({
+      input
+    });
+  }
+    
+  handleSubmit(event) {
+    let childElement = this.ChildElement.current;
+    event.preventDefault();
+    if(this.validate()){
+        console.log(this.state);
+        let input = {};
+        childElement.setState({optionSelected:null});
+        input["sujet"] = "";
+        input["detail"] = "";
+        this.setState({input:input});
+  
+        alert('Is Submit');
     }
-    this.setState({validated:true});
   }
   async creationDemande(){
     const requestOptions = {
@@ -42,6 +54,35 @@ class DemandeFormation extends Component {
       }
     });
   }
+  validate(){
+      let childElement = this.ChildElement.current;
+      let input = this.state.input;
+      let errors = {};
+      let isValid = true;
+
+      if (!input["sujet"]) {
+        isValid = false;
+        errors["sujet"] = "Renseigner un sujet.";
+      }
+  
+      if (!input["detail"]) {
+        isValid = false;
+        errors["detail"] = "Renseigner les détails de la demande.";
+      }
+      if(childElement.state.optionSelected==null){
+        isValid = false;
+        errors["domaines"] = "Renseigner au moins un domaine.";
+      }
+      this.setState({
+        errors: errors
+      });
+
+      if(isValid){
+        this.setState({showModalConfirmation:true})
+      }
+  
+      return isValid;
+  }
   renderModalConfirmation(){
     return(
       <Modal size="md" show={this.state.showModalConfirmation} onHide={()=>this.setState({showModalConfirmation:false})}>
@@ -52,70 +93,70 @@ class DemandeFormation extends Component {
                 Voulez-vous confirmer la demande de formation ?
         </Modal.Body>
         <Modal.Footer>
-            <Button type="submit" className="justify-content-center align-items-center" variant="outline-success" size="lg"   onClick={()=> {this.creationDemande();this.setState({showModalConfirmation:false})}}>Valider</Button>{' '}
+            <Button type="submit" className="justify-content-center align-items-center" variant="outline-success" size="lg"   onClick={(e)=> {this.handleSubmit(e);this.setState({showModalConfirmation:false})}}>Valider</Button>{' '}
             <Button className="justify-content-center align-items-center" variant="outline-danger" size="lg" onClick={()=>this.setState({showModalConfirmation:false})}>Annuler</Button>
         </Modal.Footer>
       </Modal>
     )
   }
   render() {
-    return(
-      <div className="DemandeFormation">
-        <div className="row justify-content-md-center  mt-3">
-          <div className="col col-lg-5 border border-dark">
-            <h1 className="justify-content-center align-items-center">
+    return (
+      <div class="DemandeFormation">
+        {this.renderModalConfirmation()}
+        <div class="row justify-content-md-center  mt-3">
+          <div class="col col-lg-5 border border-dark">
+            <h1 class="justify-content-center align-items-center">
               <u>DEMANDE DE FORMATION</u>
             </h1>
           </div>
         </div>
-        <div className="row justify-content-md-center  mt-3">
-          <div className="col col-lg-5">
-            {
-            this.renderModalConfirmation()
-            }
-            <Form noValidated validated={this.state.validated}  onSubmit={(e) => this.handleClickForm(e)}>
-              <Form.Label htmlFor="inputSujet">Sélectionnez le type de formation</Form.Label>
-              <br></br>
-              {['radio'].map((type) => (
-              <div key={`inline-${type}`} className="mb-3">
-                <Form.Check inline label="Atelier" value="atelier" name="group1" type={type} id={`inline-${type}-1`} onChange={(e)=>this.setState({selectedOption:e.target.value})}/>
-                <Form.Check inline label="Formation" value="formation" name="group1" type={type} id={`inline-${type}-2`} onChange={(e)=>this.setState({selectedOption:e.target.value})}/>
+        <div class="row justify-content-md-center  mt-3">
+          <div class="col col-lg-5">
+            <form>
+              <div class="form-group">
+                <label for="name" class="mt-2 mb-2">Indiquez le ou les domaines de formation ?</label>
+                <Select ref={this.ChildElement}/>
+                <div class="text-danger">{this.state.errors.domaines}</div>
               </div>
-              ))}
-              <Form.Group className="mt-3">
-                <Form.Label>Quel(s) serai(en)t les domaines de la formation ?</Form.Label>
-                <Radio/>
-              </Form.Group>
-              <Form.Group className="mt-3">
-                  <Form.Label htmlFor="inputSujet">Indiquez le sujet de la formation</Form.Label>
-                  <Form.Control required type="text" id="inputSujet" placeholder="Ex : Trésorie" value={this.state.sujet} onChange={(e)=>this.setState({sujet:e.target.value})} />
-                  <Form.Control.Feedback type="invalid">Renseigner un sujet de formation</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mt-3">
-                  <Form.Label>Ajoutez des détails sur votre demande de formation</Form.Label>
-                  <Form.Control required as="textarea" rows={5} value={this.state.detailDemande} onChange={(e)=>this.setState({detailDemande:e.target.value})}/>
-                  <Form.Control.Feedback type="invalid">Ajouter des détails</Form.Control.Feedback>
-              </Form.Group>
-              <Container>
-                <Row className="justify-content-md-center">
-                  <Col md="auto">
-                    <Button className="mt-3 d-flex" variant="outline-primary" onClick={() => this.setState({ showModalConfirmation: true })}>
-                      Valider
-                    </Button>
-                  </Col>
-                </Row>
-              </Container>
-            </Form>
+              <div class="form-group">
+                <label for="sujet" class="mt-2">Indiquez le sujet de la formation</label>
+                <input
+                  type="text" 
+                  name="sujet" 
+                  value={this.state.input.sujet}
+                  onChange={this.handleChange}
+                  class="form-control mt-2" 
+                  placeholder="Ex : Trésorie" 
+                  id="email" />
+                  <div class="text-danger">{this.state.errors.sujet}</div>
+              </div>
+              <div class="form-group">
+                <label for="detail" class="mt-2">Ajoutez des détails sur votre demande de formation</label>
+                <textarea 
+                  name="detail"
+                  value={this.state.input.detail} 
+                  onChange={this.handleChange}
+                  placeholder="Date, déroulement, pré-requis, ..."
+                  class="form-control mt-2" 
+                  rows="7" />
+                  <div class="text-danger">{this.state.errors.detail}</div>
+              </div>
+              <div class="row mt-2 justify-content-center align-items-center">
+                <div class="col col-lg-2">
+                    <input type="button" value="Valider" class="btn btn-primary" onClick={()=> {this.validate()}}/>
+                  </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default DemandeFormation;
 
-class Radio extends Component {
+class Select extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -159,7 +200,6 @@ class Radio extends Component {
           onChange={this.handleChange}
           allowSelectAll={true}
           value={this.state.optionSelected}
-          required
         />
       </span>
     );
