2c5304c96bfe37e29330e99c6d36064b1d743458
DiakHadja <hadjafatoumatadiakite@gmail.com>
2022-04-12 16:50:22 +0200

    Fiche de formation, afichage des information. pluging and disign (dependance)

--- src/VueDetailleeFormation/InformationsFicheDeFormation.js
+++ src/VueDetailleeFormation/InformationsFicheDeFormation.js
@@ -1,139 +1,143 @@
-import React, {Component} from 'react';
-import {AiOutlinePlusCircle} from 'react-icons/ai';
-import {IconContext} from "react-icons";
-
-class InformationsFicheDeFormation extends Component {
+import React, {} from 'react';
+import Donnee from '../VueDetailleeFormation/json/InformationFicheDeFormation.json';
 
 
-    render() {
+
+
+const AfficherType = Donnee.data.map(
+    (info) => {
         return (
+            <p key={info.id}>
+                <td>{info.type}</td>
+            </p>
+        )
+    }
+)
 
-            <div className="container-fluid mt-10" className="main">
+const AfficherBesoinsMaterielsFormation = Donnee.data[0].BesoinsMaterielsFormation.map(
+    (info) => {
+        return (
+            <tr key={info.id}>
+     
+                <li>{info.nom }</li>
+            </tr>
+        )
+    }
+)
 
-                <div className="row  mb-3">
-                    <h2 className="mt-2">
-                        <ins>Information</ins>
-                    </h2>
+const AfficherobjetsPedagogiques = Donnee.data[0].objetsPedagogiques.map(
+    (info) => {
+        return (
+            <tr key={info.id}>
+                <td>{info.id}</td>
+                <td>{info.objet }</td>
+            </tr>
+        )
+    }
+)
 
-                </div>
+const AfficherDataDomaine = Donnee.data[0].domaines.map(
+    (info) => {
+        return (
+            <tr key={info.code} title={info.description}>
+                <td>{info.code}</td>
+                <td>{info.libelle}</td>
+            </tr>
+        )
+    }
+)
 
-                <div className="container-fluid shadow p-3 mb-3 bg-white rounded">
-
+function InformationsFicheDeFormation (){
+  
+    return(
+        <>
+        <div className="container shadow p-4 mb-3 bg-white rounded">
+            <div className="row">
+                <h3 className="mt-2">
+                   Type
+                </h3>
+                <br/>
+                <div>
                     <div className="row  mb-3">
                         <div className="col-2">
-                            <h4 className="mb-3">
-                                <ins>Type</ins>
-                            </h4>
-                            <select className="form-select form-select-sm" aria-label=".form-select-sm example">
-                                <option value="1">Formation</option>
-                                <option value="2">Ateler</option>
-                            </select>
+                           
+                           <h4 style={{marginLeft: '15px' }}>{AfficherType}</h4>
                         </div>
                     </div>
                 </div>
-                <div className="container-fluid shadow p-3 mb-3 bg-white rounded">
-                    <div className="row  mb-3">
+         
+                <div className="row  mb-3" >
+                <h3 className="mt-2">
+                   Objectifs pédagogiques
+                </h3>
+                   
+                    <div className="col-6">
+                        <div className="container">
+                            <div className="table-wrapper">
+                                <table className="table table-striped mt-2">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Objectifs</th>
+                                        </tr>
+                                    </thead>
 
-                        <h4>
-                            <ins>Objectif Pédagogiques</ins>
-                        </h4>
-                        <p className="mb-3"><small><em>(a l'issue de la formation, les stagiaires seront capable de
-                            ...)</em></small></p>
-                        <div className="col-sm-4">
-                            <button type="button" className="btn btn-outline-secondary">
-                                <IconContext.Provider value={{className: "AiOutlinePlusCircle-icon"}}>
-                                    <AiOutlinePlusCircle/>
-                                </IconContext.Provider>Ajouter une ligne
-                            </button>
-                        </div>
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Objectif</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">1.</th>
-                                <td>
-                                    <input type="text" className="form-text" placeholder="A déterminer"/>
-                                </td>
-                                <td>
-                                    <a href="#editEmployeeModal" className="edit" data-toggle="modal">
-                                        <img src={require('../Img/delete.png')} className="Icones"/>
-                                    </a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-
-
-                    </div>
-                </div>
-                <div className="container-fluid shadow p-1 mb-3 bg-white rounded">
-                    <div className="row  mb-3">
-
-                        <h4 className="mb-3">
-                            <ins>Domaine de formation</ins>
-                        </h4>
-                        <div className="col-sm-4">
-                            <button type="button" className="btn btn-outline-secondary">
-                                <IconContext.Provider value={{className: "AiOutlinePlusCircle-icon"}}>
-                                    <AiOutlinePlusCircle/>
-                                </IconContext.Provider>Ajouter une ligne
-                            </button>
-                        </div>
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Objectif</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">1.</th>
-                                <td>
-                                    <input type="text" className="form-text" placeholder="A déterminer"/>
-                                </td>
-                                <td>
-                                    <a href="#editEmployeeModal" className="edit" data-toggle="modal">
-                                        <img src={require('../Img/delete.png')} className="Icones"/>
-                                    </a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div className="container-fluid shadow p-2 bg-white rounded">
-                    <div className="row  mb-3">
-                        <div className='col-5'>
-                            <h4 className="mb-3">
-                                <ins>Besoins matériels de la formation</ins>
-                            </h4>
-                            <div className="card">
-                                <ul className="panel">
-                                    <li>Tableau</li>
-                                    <li>Projecteur</li>
-                                    <li>Feutres</li>
-                                    <li>Capotes</li>
-                                    <li>La teub de théo</li>
-                                </ul>
+                                    <tbody>
+                                        {AfficherobjetsPedagogiques}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
+            
+                    
                 </div>
-                <div className="row mt-3"></div>
             </div>
+        </div>
+        <div className="container shadow p-4 mb-3 bg-white rounded">
+            <div className="row">
+                <div className="col-6">
+                    <h3 className="mt-2">
+                        Domaines
+                    </h3>
+                
+                    <div className="col-6" style={{width: 600}}>
+                        <div className="container">
+                            <div className="table-wrapper">
+                                <table className="table table-striped mt-2">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Nom</th>
+                                        </tr>
+                                    </thead>
 
-
-        )
-    }
-
+                                    <tbody>
+                                        {AfficherDataDomaine}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+            
+                    
+                </div>
+                <div className="col-6">
+                            <h3 className="mt-2">
+                               Besoins matériel 
+                            </h3>
+                            <br/>
+                            <div className="card">
+                                <ul className="panel" style={{border: 0}}>
+                                    <li>{AfficherBesoinsMaterielsFormation}</li>
+                                </ul>
+                            </div>
+                        </div>
+            </div>
+            </div>
+                <div className="row mt-3"></div>
+       
+        </>
+    );
 }
-
-export default InformationsFicheDeFormation;
+export default InformationsFicheDeFormation ;
