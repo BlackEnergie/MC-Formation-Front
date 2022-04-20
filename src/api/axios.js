0fa4e9d0070252cd11f4190cf44be1d9ced1205f
Théo Perrin <the.perrin@outlook.fr>
2022-04-20 21:47:32 +0200

    Url back dépendante du hostname

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = "http://" + window.location.hostname + ":8080";
 
 export default axios.create({
     baseURL: BASE_URL
