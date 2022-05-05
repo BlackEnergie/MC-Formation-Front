7cd6af988358db7ab7b0526f5e5ab82be15a2114
Julien <j.dubert12@gmail.com>
2022-05-05 11:46:32 +0200

    correction

--- src/serverInteraction/common.ts
+++ src/serverInteraction/common.ts
@@ -2,7 +2,7 @@ export const BASE_URL = "http://" + window.location.hostname + ":8080";
 
 export enum EntryPoint {
     ALL_FORMATION = '/formations',
-    FORMATION_BY_ID = '/formation',
+    FORMATION_BY_ID = '/formation/',
     GENERAL_SIGN_UP = '/auth/signup/checkToken?token=',
     CHECK_TOKEN = '/auth/signup/checkToken?token=',
     INSCRIPTION_URL = '/auth/signup/create?token=',
