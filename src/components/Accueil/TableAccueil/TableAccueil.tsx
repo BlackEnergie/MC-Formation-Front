ba519c84e9a312255f1c3656947ef3f693500fc6
Julien <j.dubert12@gmail.com>
2022-05-10 16:26:00 +0200

    formatage

--- src/components/Accueil/TableAccueil/TableAccueil.tsx
+++ src/components/Accueil/TableAccueil/TableAccueil.tsx
@@ -1,97 +1,183 @@
-import React, {useEffect,} from "react";
-import tableSort from "table-sort-js/table-sort.js";
+import React from "react";
 import {AiOutlineEdit, AiOutlineZoomIn} from "react-icons/ai";
 import {Link} from "react-router-dom";
 import {statutToString, statutToStyle} from "../../../utils/StatutUtils";
 import decodeToken from "../../../auth/decodeToken";
+import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow } from "@mui/material";
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-const TableAccueil = ({Donnee}) => {
-
-    useEffect(() => {
-        tableSort()
-    }, []);
-
-
-    const checkRoleAsso = () => {
-        const token = decodeToken(localStorage.getItem("accessToken")).decoded;
-        return token.role === "ROLE_ASSO"
-    }
-
-    const domaineLibelleList = (domaines) => {
-        let list = [];
-        domaines.map((domaine) => {
-            list.push(domaine.libelle)
-        })
-        return list;
-    }
-
-
-    const DisplayData = () => {
-        const display = Donnee.map((info) =>
-            <tr key={info.id}>
-                <td className={statutToStyle(info.statut)}>{statutToString(info.statut)}</td>
-                <td>{info.cadre ? info.cadre : "N/A"}</td>
-                <td>{domaineLibelleList(info.domaines).join(", ")}</td>
-                <td>{info.titre ? info.titre : "Provisoire : " + info.sujet}</td>
-                <td title={info.association.nomComplet}>{info.association.acronyme}</td>
-                <td>{info.formateurs.length > 0 ? info.formateurs.map((formateur) => formateur.nomComplet) : "Aucun"}</td>
-                <td>{info.date ? info.date : "N/A"}</td>
-                <td className="text-center" key={info.id}>
-                    <Link to={'/formation/' + info.id}>
-                        <AiOutlineZoomIn className="Icones me-2"/>
-                    </Link>
-                    {checkRoleAsso() ?
-                        (<></>) :
-                        (<Link to={'/formation/edit/' + info.id}>
-                            <AiOutlineEdit className="Icones me-2"/>
-                        </Link>)}
-                </td>
-            </tr>
-        )
-        return (
-            <>
-                {
-                    Donnee.length > 0 ? display : <tr>No data</tr>
-                }
-            </>
-        )
-    }
-
-    const nbFormations = Donnee.length;
-
+interface formation {
+    association: {
+      acronyme: string;
+      nomComplet: string;
+    };
+    cadre?: string;
+    domaines: {
+      code: string;
+      libelle: string;
+      description: string;
+    }[];
+    formateurs: {
+      id: number;
+      nom: string;
+      prenom: string;
+    }[];
+    statut: Number;
+    titre?: string;
+    sujet: string;
+    date?: string;
+    id: number;
+  }
+  
+  interface TablePaginationActionsProps {
+    count: number;
+    page: number;
+    rowsPerPage: number;
+    onPageChange: (
+      event: React.MouseEvent<HTMLButtonElement>,
+      newPage: number,
+    ) => void;
+  }
+  
+  function TablePaginationActions(props: TablePaginationActionsProps) {
+    const { count, page, rowsPerPage, onPageChange } = props;
+  
+    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      onPageChange(event, page - 1);
+    };
+  
+    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      onPageChange(event, page + 1);
+    };
+  
     return (
-        <>
-            <div className="row">
-                <div className="col p-0">
-                    <h3 className="form-label color-mc">Formations</h3>
-                </div>
-                <div className="col text-end">
-                    <div className="hint-text"><b>{nbFormations}</b> résultat{nbFormations>1?"s":""}</div>
-                </div>
-            </div>
-            <hr/>
-            <div className="table-wrapper" id="tableAccueil">
-                <table className="table table-striped mt-2 table-sort table-arrows">
-                    <thead>
-                    <tr>
-                        <th scope="col">Statut</th>
-                        <th scope="col">Cadre</th>
-                        <th scope="col">Domaine(s)</th>
-                        <th scope="col">Titre</th>
-                        <th scope="col">Association</th>
-                        <th scope="col">Formateur(s)</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {DisplayData()}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          <KeyboardArrowLeft />
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+           <KeyboardArrowRight />
+        </IconButton>
+      </Box>
+    );
+  }
+  
+  function TableAccueil(data: formation[]) {
+  
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  
+    const domaineLibelleList = (domaines) => {
+      let list = [];
+      domaines.map((domaine) => {
+          list.push(domaine.libelle)
+      })
+      return list;
+    }
 
-}
-
+    data.sort((a, b) => a.id - b.id)
+    
+    const checkRoleAsso = () => {
+      const token = decodeToken(localStorage.getItem("accessToken")).decoded;
+      return token.role === "ROLE_ASSO"
+    }
+    
+    const checkRoleBn = () => {
+      const token = decodeToken(localStorage.getItem("accessToken")).decoded;
+      return token.role === "ROLE_BN"
+    }
+    
+    const checkRoleFormateur = () => {
+      const token = decodeToken(localStorage.getItem("accessToken")).decoded;
+      return token.role === "ROLE_FORMATEUR"
+    }
+  
+    const handleChangePage = (
+      event: React.MouseEvent<HTMLButtonElement> | null,
+      newPage: number,
+    ) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+  
+  
+    return (
+      <>
+        <div className="container-fluid" id="accueil">
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableCell align="center">Statut</TableCell>
+                <TableCell align="center">Cadre</TableCell>
+                <TableCell align="center">Domaine(s)</TableCell>
+                <TableCell align="center">Titre</TableCell>
+                <TableCell align="center">Association</TableCell>
+                <TableCell align="center" hidden={!checkRoleBn()}>Formateur(s)</TableCell>
+                <TableCell align="center">Date</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableHead>
+              <TableBody>
+                {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                  <TableRow key={row.id}>
+                    <TableCell align="center" style={{ color : statutToStyle(row.statut)}}>{statutToString(row.statut)}</TableCell>
+                    <TableCell align="center">{row.cadre == null ? "N/A" : row.cadre}</TableCell>
+                    <TableCell align="center">{domaineLibelleList(row.domaines).join(", ")}</TableCell>
+                    <TableCell align="center">{row.titre != null ? row.titre : "Provisoire : " + row.sujet}</TableCell>
+                    <TableCell align="center" title={row.association.nomComplet}>{row.association.acronyme}</TableCell>
+                    <TableCell align="center" hidden={!checkRoleBn()}>{row.formateurs.map((formateur) => formateur.prenom + " " + formateur.nom)}</TableCell>
+                    <TableCell align="center">{row.date == null ? "N/A" : row.date}</TableCell>
+                    <TableCell align="center" >
+                      <Link to={'/formation/' + row.id}>
+                          <AiOutlineZoomIn className="Icones me-2"/>
+                      </Link>
+                      {checkRoleAsso() ?
+                          (<></>) :
+                          (<Link to={'/formation/edit/' + row.id}>
+                              <AiOutlineEdit className="Icones me-2"/>
+                          </Link>)}
+                      </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow >
+                  <TablePagination
+                  rowsPerPageOptions={[5, 10, 15]}
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      'aria-label': 'Lignes par page',
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                  align="right"
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+        </div>
+      </>
+    );
+  }
 export default TableAccueil;
