94be66c178e08c472d1bbfbb221dcd9ad9460c7e
Alexandru <mrbeardman1@gmail.com>
2022-05-19 16:47:31 +0200

    Modification Formation + Fil Conducteur + Fiche de Formation fonctionnelles

--- src/components/formation/ModificationFormation/ModificationFormation.tsx
+++ src/components/formation/ModificationFormation/ModificationFormation.tsx
@@ -3,6 +3,7 @@ import ModificationFicheDeFormation from "./ModificationFicheDeFormation";
 import ModificationFilConducteur from "./ModificationFilConducteur";
 import {useParams} from "react-router-dom";
 import {FetchFormationById} from '../../../serverInteraction/FetchFormation';
+import {FetchDomaines} from "../../../serverInteraction/FetchData";
 import useAxiosPrivate from '../../../auth/hooks/useAxiosPrivate';
 import Formation from '../../../api/model/Formation';
 import {Accordion, AccordionDetails, AccordionSummary, Container, Skeleton, Typography} from '@mui/material';
@@ -11,21 +12,36 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ModificationInformationsGenerales from "./ModificationInformationsGenerales";
 import { useNavigate } from "react-router-dom";
 import toast from 'react-hot-toast';
+import {domaines} from "../../Accueil/ComposantAccueil/FiltreAccueil";
 
 const ModificationFormation = () => {
     const INITIAL_FORMATION: Formation = new Formation();
+    const INITIAL_DOMAINE : domaines[] = [];
     let {id} = useParams();
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
     const [formation, setFormation] = useState(INITIAL_FORMATION);
     const [loading, setLoading] = useState(true);
+    const [domaine, setDomaine] = useState(INITIAL_DOMAINE);
 
     useEffect(() => {
         getFormationDetails().then(data => setFormation(data));
+        getDomaineList();
     }, [])
 
     const majFormation = (newFormation) => {
         setFormation(newFormation);
+    }
+
+    const getDomaineList = async () => {
+
+        try {
+            const controller = new AbortController();
+            const response = await FetchDomaines(axiosPrivate,controller);
+            setDomaine(response?.data);
+        }catch (err){
+            console.log(err)
+        }
     }
 
     const getFormationDetails = async () => {
@@ -101,9 +117,8 @@ const ModificationFormation = () => {
                             </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-                            <ModificationFicheDeFormation/>
-                            <ModificationFilConducteur formation={formation}
-                                                       majFormation={majFormation}/>
+                            <ModificationFicheDeFormation formation={formation} majFormation={majFormation} domaine={domaine} />
+                            <ModificationFilConducteur formation={formation} majFormation={majFormation}/>
                         </AccordionDetails>
                     </Accordion>
                 </>
